d950623cfda8f2bf8d941dd6bc4f551b4e949e09
LirraSmile <nad.zyryanova@gmail.com>
2019-12-03 18:42:30 +0100

    Modify test_mainpage.py

--- test_main_page.py
+++ test_main_page.py
@@ -1,8 +1,13 @@
+from .pages.main_page import MainPage
+
 link = "http://selenium1py.pythonanywhere.com/"
 
-
-
 def test_guest_can_go_to_login_page(browser):
-    browser.get(link)
-    go_to_login_page(browser)
+    #инициализация Page Object, передача в конструктор 
+    # экземпляра драйвера и url-адреса
+    page = MainPage(browser, link)
+    #открыть страницу
+    page.open()
+    #выполнить метод страницы - переход на страницу резистрация/логин
+    page.go_to_login_page()
     

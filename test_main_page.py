f594e4d1ed77759d349ee14c9bff6b002a3f1963
LirraSmile <nad.zyryanova@gmail.com>
2019-12-07 09:06:47 +0100

    Add tests for login page. Reaalization LoginPage. Add locators

--- test_main_page.py
+++ test_main_page.py
@@ -1,4 +1,5 @@
 from .pages.main_page import MainPage
+from .pages.login_page import LoginPage
 
 link = "http://selenium1py.pythonanywhere.com/"
 
@@ -10,5 +11,7 @@ def test_guest_can_go_to_login_page(browser):
     page.open()
     #выполнить метод страницы - переход на страницу резистрация/логин
     page.should_be_login_link()
-    #page.go_to_login_page()
+    page.go_to_login_page()
+    login_page = LoginPage(browser, browser.current_url)
+    login_page.should_be_login_page()
     

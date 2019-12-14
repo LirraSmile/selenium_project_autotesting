235d6c6aa27d65ce676a15ad801d91cf8f017131
LirraSmile <nad.zyryanova@gmail.com>
2019-12-14 17:09:24 +0100

    Modify conftest.py, added simple script test_screenshots.py for screen capture testing

--- conftest.py
+++ conftest.py
@@ -2,6 +2,7 @@ import pytest
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options #для указания локализации
 import time
+from datetime import datetime
 
 #передача параметров в командной строке
 def pytest_addoption(parser):
@@ -31,7 +32,12 @@ def browser(request):
         browser = webdriver.Firefox(firefox_profile=fp)
     else:
         raise pytest.UsageError("--browser_name should be chrome or firefox")
+    browser.implicitly_wait(10)
     yield browser
     print("\nquit browser..")
-    time.sleep(10)
+    #time.sleep(10)
+    # получаем переменную с текущей датой и временем в формате ГГГГ-ММ-ДД_ЧЧ-ММ-СС
+    now = datetime.now().strftime('%Y-%m-%d_%H-%M-%S')
+    # делаем скриншот с помощью команды Selenium'а и сохраняем его с именем "screenshot-ГГГГ-ММ-ДД_ЧЧ-ММ-СС"
+    browser.save_screenshot('Screenshots/screenshot-%s.png' % now)
     browser.quit()
